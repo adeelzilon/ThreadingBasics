bebac7c8e1f069d369d2374d6ca9a8f40be2db0d
adeelzilon <engr.adeelnoshahi@gmail.com>
2024-01-09 20:14:51 +0500

    Made 4 Async methods

--- Program.cs
+++ Program.cs
@@ -3,5 +3,34 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Zilon International Inc.");
+        SyncTask1();
+        SyncTask2();
+        SyncTask3();
+        SyncTask4();
+
+    }
+
+    public static void SyncTask1()
+    {
+        Console.WriteLine("SyncTask1 is starting");
+        Thread.Sleep(5000);
+        Console.WriteLine("SyncTask1 is completed");
+
+    }
+
+    public static void SyncTask2()
+    {
+        Console.WriteLine("SyncTask2 is starting");
+        Thread.Sleep(2000);    
+        Console.WriteLine("SyncTask2 is completed");
+
+    }
+
+    public static void SyncTask3()
+    {
+        Console.WriteLine("SyncTask3 is starting");
+        Thread.Sleep(3000);
+        Console.WriteLine("SyncTask3 is completed");
+
     }
 }
